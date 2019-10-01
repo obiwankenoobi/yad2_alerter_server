5c08d3fded3c0a7e01af2c44ad09bd95464cd26b
obivankanoobi <artium1@gmail.com>
2019-10-01 17:16:52 +0300

    email module added

---
+++ services/email/index.js
@@ -0,0 +1,98 @@
+'use strict';
+const nodemailer = require('nodemailer');
+
+/**
+ * the email object to send
+ * @typedef {EmailObject}  
+ * @property {String} fromEmail 
+ * @property {String} toEmail
+ * @property {String} subject
+ * @property {String} text
+ * @property {String} html
+ */
+/**
+ * function to send an email
+ * @param {EmailObject} emailObj 
+ */
+async function sendEmail(emailObj) {
+
+  const { 
+    fromEmail,
+    toEmail,
+    subject, 
+    text,
+    html
+  } = emailObj
+
+  // create reusable transporter object using the default SMTP transport
+  const transporter = nodemailer.createTransport({
+      host: 'smtp.office365.com',
+      port: 587,
+      secure: false, // true for 465, false for other ports
+      auth: {
+          user: process.env.SMTP_EMAIL ,  // generated ethereal user
+          pass: process.env.SMTP_PASSWORD // generated ethereal password
+      }
+  });
+
+  console.log('process.env.SMTP_EMAIL', process.env.SMTP_EMAIL)
+  // send mail with defined transport object
+  const info = await transporter.sendMail({
+      from: fromEmail, // sender address
+      to: toEmail, // list of receivers
+      subject, // Subject line
+      text, // plain text body
+      html // html body
+  });
+
+  console.log('Message sent: %s', info.messageId);
+  // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+
+  // Preview only available when sending through an Ethereal account
+  console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+  // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+}
+
+
+/**
+ * function to send th results the crawler found among the registereed emails
+ * @param {Object} results 
+ * @example
+ * {
+ *  "2038722002": {
+ *    "foundLinks": [],
+ *    "emails": {
+ *      "artium1new@gmail.com": true
+ *    }
+ *  },
+ *  "2084409008": {
+ *    "foundLinks": [],
+ *    "emails": {
+ *      "artium1@gmail.com": true
+ *    }
+ *  }
+ * }
+ */
+function sendLinks(results) {
+  for(let hash in results) {
+    for(let email in results[hash].emails) {
+      const linksFound = results[hash].foundLinks
+
+      if (!linksFound.length) return 
+
+      const emailObj = {
+        fromEmail:'dev@inlyne.co',
+        toEmail: email,
+        subject:'hello world', 
+        text: linksFound.map(id => 'https://www.yad2.co.il/item/'.concat(id) + '\n').toString().replace(/,/g, ''),
+        html:''
+      }
+      sendEmail(emailObj).catch(console.error)
+    }
+  }
+} 
+
+
+
+
+module.exports = { sendEmail, sendLinks }
