f224f25f558c5a7f8aa0c4daa17ba439497bb74c
obivankanoobi <artium1@gmail.com>
2019-10-03 11:44:28 +0300

    remove unused route

--- api/alerts.js
+++ api/alerts.js
@@ -1,9 +1,8 @@
 const { addAlert } = require('../models/alerts')
-const { main } = require('../models/main')
 const express = require("express")
 const router = express.Router()
 
 router.post('/add', addAlert)
-router.post('/crawl', main)
+
 
 module.exports = router
